6a66366cb61bb5612af935ef4cf7dbef5ba40a88
tnmquan9799 <tnmquan9799@gmail.com>
2018-11-28 16:43:18 +0700

    ConnectionFactory

---
+++ src/vn/edu/vnuk/jdbc/ConnectionFactory.java
@@ -0,0 +1,20 @@
+package vn.edu.vnuk.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public Connection getConnection() {
+		try {
+			return DriverManager.getConnection(
+					"jdbc:mysql://localhost/vnuk_personnel", 
+					"root", 
+					"Mquan123"
+			);
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
